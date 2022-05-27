78f7065fae5bba77b55e1f874d68ddd623b827a6
Marty <jmjmjm373@gmail.com>
2022-05-28 00:17:19 +0800

    Fix 16x16 grid UI and Add a change grid button

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 
 
-function grid() {
+function defaultGrid() {
 
     const mainContainer = document.querySelector('.main-container');
 
-
-
-    if (arguments.length == 0) {
-        //grid layout 16x16
-        for (let j = 1; j <= 16; j++) {
-            for (let i = 1; i <= 16; i++) {
-                let nameOfGrid = `divGrad${i}`
-                nameOfGrid = document.createElement('div');
-                nameOfGrid.setAttribute('class', 'grid');
-                nameOfGrid.setAttribute('style',
+    //grid layout 16x16
+    for (let j = 1; j <= 16; j++) {
+        for (let i = 1; i <= 16; i++) {
+            let nameOfGrid = `divGrad${i}`
+            nameOfGrid = document.createElement('div');
+            nameOfGrid.setAttribute('class', 'grid');
+            nameOfGrid.setAttribute('style',
                 'width: 60px; height: 60px;'
-                );
-                mainContainer.appendChild(nameOfGrid);
+            );
+            mainContainer.appendChild(nameOfGrid);
 
-            }
         }
-        return;
-    } else {
-        return;
-        //get the input grid size then boom
     }
+    return;
+
 }
 
-grid();
+
+defaultGrid();
+const customGridButton = document.querySelector('.btn-container');
+customGridButton.addEventListener('click', () => {
+    //code for changing the grid size and removing default.
+    // const grids = document.querySelector('.main-container').childNodes;
+    // console.log(grids);
+    // console.log(grids.length);
+    // for (let j = 1; j <=grids.length; j++) {
+
+    //     for (let i = 1; i <=grids.length; i++) {
+    //         grids[i].remove();
+    //     }
+    // }
+    // console.log(grids.length);
+    // console.log(grids);
+    location.reload();
+});
+
+
+
+
+
+
