f2c9d697e34311a6579807b6366cbb1db0732341
Marty <jmjmjm373@gmail.com>
2022-05-30 16:52:30 +0800

    Add function to get gride size and clear the grid.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 
-function defaultGrid() {
+function initialGrid() {
 
     const mainContainer = document.querySelector('.main-container');
 
-    //grid layout 16x16
+    //create a grid layout 16x16
     for (let j = 1; j <= 16; j++) {
         for (let i = 1; i <= 16; i++) {
             let nameOfGrid = `divGrad${i}`
@@ -21,23 +21,60 @@ function defaultGrid() {
 
 }
 
+function getGridSize() {
+    let msg = 'Please enter a valid number';
+    let size = prompt('Please enter the new grid size(limit is 100):');
+    if ((size == null) || (size == '') || (isNaN(size))) {
+        alert(msg);
+    } else {
+        console.log(size);
+        return size
+    }
 
-defaultGrid();
+}
+
+function clearGrid() {
+
+    const mainContainer = document.querySelector('.main-container');
+    while (mainContainer.firstElementChild) {
+        mainContainer.firstElementChild.remove();
+    }
+}
+
+
+function drawGrid(size) {
+
+    const mainContainer = document.querySelector('.main-container');
+
+    //create a grid layout based on size
+    for (let j = 1; j <= size; j++) {
+        for (let i = 1; i <= size; i++) {
+            let nameOfGrid = `divGrad${i}`
+            nameOfGrid = document.createElement('div');
+            nameOfGrid.setAttribute('class', 'grid');
+            nameOfGrid.setAttribute('style',
+                'width: 60px; height: 60px;'
+            );
+            mainContainer.appendChild(nameOfGrid);
+
+        }
+    }
+    return;
+
+}
+
+
+initialGrid();
+const grids = document.querySelector('.main-container').childNodes;
+console.log(grids);
+console.log(grids.length);
+
 const customGridButton = document.querySelector('.btn-container');
 customGridButton.addEventListener('click', () => {
-    //code for changing the grid size and removing default.
-    // const grids = document.querySelector('.main-container').childNodes;
-    // console.log(grids);
-    // console.log(grids.length);
-    // for (let j = 1; j <=grids.length; j++) {
+    clearGrid();
+    drawGrid(getGridSize());
 
-    //     for (let i = 1; i <=grids.length; i++) {
-    //         grids[i].remove();
-    //     }
-    // }
-    // console.log(grids.length);
-    // console.log(grids);
-    location.reload();
+    //location.reload();
 });
 
 
